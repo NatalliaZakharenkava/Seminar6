5eee13357b505a10d4fc69b9f416fedd1810b5df
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-06-19 08:45:10 +0300

    add new task

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        MySet<Integer> numbers = new MySet<>();
+
+        for (int i = 0; i < 10; i++) {
+            numbers.addNum(new Random().nextInt(10));
+        }
+
+
+        Iterator<Integer> iter = numbers.iterator();
+        while (iter.hasNext()) {
+            int a = iter.next();
+            if (a % 2 == 0){
+                System.out.println(a);
+            }
+        }
+
+
+        System.out.println(numbers);
+    }
+
+}
+
+class MySet<T> {
+
+    private HashMap<T, Object> myMap = new HashMap<>();
+    private static final Object MYOBJECT = new Object();
+
+    public void addNum(T i) {
+
+        myMap.put(i, MYOBJECT);
+
+    }
+
+    public boolean remove(T i) {
+
+        return myMap.remove(i) == MYOBJECT;
+    }
+
+    public boolean contains(T i) {
+
+        return myMap.containsKey(i);
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder str = new StringBuilder("[");
+        Iterator<T> iter = myMap.keySet().iterator();
+
+        while (iter.hasNext()){
+            str.append(iter.next() + ",");
+        }
+        str.append("]");
+
+        return str.toString();
+    }
+
+    public Iterator <T> iterator(){
+        return myMap.keySet().iterator();
+    }
+
+}
